7c2e7072a6f08e49eae04d7f4f4b9d5dff0d0637
GEORGE Pompilio <George@georges-mbp-2.myfiosgateway.com>
2020-03-21 12:37:26 -0400

    init commit

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 
 const app = express()
 
+var Vimeo = require('vimeo').Vimeo;
+var client = new Vimeo(CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN)
+
 app.use(express.static('./public'))
 app.set('view engine', 'hbs')
 
